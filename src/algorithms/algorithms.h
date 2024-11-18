0e3309580dd27b57c497b8ff7d60595e563b5536
Lion Doge <KrystianoXPL@gmail.com>
2024-11-18 16:38:53 +0100

    Add BFS algorithm

--- src/algorithms/algorithms.h
+++ src/algorithms/algorithms.h
@@ -10,4 +10,8 @@ namespace Algorithms
     // and gets filled with all the nodes visited in the order they were visited
     std::vector<int> dfs(MazeGraph& mazeGraph, int startNode, int endNode,
         std::vector<int>& outNodeVisitOrder);
+
+    // usage is the same as dfs, but uses breadth first search
+    std::vector<int> bfs(MazeGraph& mazeGraph, int startNode, int endNode,
+        std::vector<int>& outNodeVisitOrder);
 }
---
+++ src/algorithms/bfs.cpp
@@ -0,0 +1,47 @@
+#include <vector>
+#include <set>
+#include <queue>
+#include "graph.h"
+#include "algorithms.h"
+
+std::vector<int> Algorithms::bfs(MazeGraph& mazeGraph, int startNode, int endNode, std::vector<int>& nodeVisitOrder)
+{
+    std::set<int> visited;
+    std::queue<int> graphQueue;
+    std::queue<std::vector<int>> pathQueue;
+    //std::vector<int> bestPath;
+    graphQueue.push(startNode);
+    pathQueue.push(std::vector<int>{startNode});
+    while(!graphQueue.empty())
+    {
+        int currentNode = graphQueue.front();
+        graphQueue.pop();
+        std::vector<int> path = pathQueue.front();
+        pathQueue.pop();
+        
+        if(currentNode == endNode)
+        {
+            visited.insert(currentNode);
+            nodeVisitOrder.push_back(currentNode);
+            return path;
+        }
+
+        if(visited.find(currentNode) == visited.end())
+        {
+            visited.insert(currentNode);
+            nodeVisitOrder.push_back(currentNode);
+            std::vector<int> adjNodes = mazeGraph.GetAdjacentNodeIndicies(currentNode);
+            for(auto neighbor = adjNodes.begin(); neighbor != adjNodes.end(); neighbor++)
+            {
+                if(visited.find(*neighbor) == visited.end())
+                {
+                    graphQueue.push(*neighbor);
+                    std::vector<int> newPath = path;
+                    newPath.push_back(*neighbor);
+                    pathQueue.push(newPath);
+                }
+            }
+        }
+    }
+    return std::vector<int>();
+}
