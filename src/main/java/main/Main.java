4c3b0f7e7941de629c2dc90068b3dc49cc30dfdf
kuba913 <43656309+kuba913@users.noreply.github.com>
2024-11-19 01:07:46 +0100

    Reworked display program

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,27 @@
+package labirynt.src.main.java.main;
+
+import java.awt.image.BufferedImage;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+    public static void main(String avg[]) {
+        BufferedImage img = new BufferedImage(100, 100, BufferedImage.TYPE_INT_RGB);
+        JFrame window=new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("labyrinth");
+
+        LabyrinthPanel labPanel = new LabyrinthPanel();
+        window.add(labPanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        labPanel.startLabThread();
+    }
+}
+
