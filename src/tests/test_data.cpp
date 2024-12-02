607d6e0b02a9b20f3be969673ef18a225770eb85
Lion Doge <KrystianoXPL@gmail.com>
2024-12-02 22:47:06 +0100

    Add MapToGraph loop testing, and adjust some existing tests

--- src/tests/test_data.cpp
+++ src/tests/test_data.cpp
@@ -33,6 +33,35 @@ TEST(MazeMapTest, Initializer)
     EXPECT_FALSE(lab.Get(2, 2));
 }
 
+TEST(MazeMapTest, MapToGraph_SimpleLoop)
+{
+    MazeMap lab(2, 2);
+    MazeGraph graph = lab.ToMazeGraph();
+
+    ASSERT_EQ(graph.GetNumVertices(), 4);
+
+    auto adj0 = graph.GetAdjacentNodeIndicies(graph.GetIndexFromCoords(0, 0));
+    // (0, 0) should be connected to (0, 1) and (1, 0)
+    ASSERT_TRUE(VEC_CONTAINS(adj0, graph.GetIndexFromCoords(1, 0)));
+    ASSERT_TRUE(VEC_CONTAINS(adj0, graph.GetIndexFromCoords(0, 1)));
+    auto adj1 = graph.GetAdjacentNodeIndicies(graph.GetIndexFromCoords(1, 1));
+    // (1, 1) should be connected to (0, 1) and (1, 0)
+    ASSERT_TRUE(VEC_CONTAINS(adj1, graph.GetIndexFromCoords(1, 0)));
+    ASSERT_TRUE(VEC_CONTAINS(adj1, graph.GetIndexFromCoords(0, 1)));
+}
+
+TEST(MazeMapTest, MapToGraph)
+{
+    MazeMap lab(3, 3, {{0, 0}, {0, 2}, {2, 1}, {2, 2}});
+    MazeGraph graph = lab.ToMazeGraph();
+
+    // (1, 1) should be connected to (0, 1), (1, 0) and (2, 1)
+    auto adj2 = graph.GetAdjacentNodeIndicies(graph.GetIndexFromCoords(1, 1));
+    ASSERT_TRUE(VEC_CONTAINS(adj2, graph.GetIndexFromCoords(1, 0)));
+    ASSERT_TRUE(VEC_CONTAINS(adj2, graph.GetIndexFromCoords(0, 1)));
+    ASSERT_TRUE(VEC_CONTAINS(adj2, graph.GetIndexFromCoords(1, 2)));
+}
+
 TEST(MazeGraphTest, AddVertex)
 {
     MazeGraph graph(0);
@@ -53,21 +82,9 @@ TEST(MazeGraphTest, AddEdge)
     EXPECT_EQ(adj[0], idx2);
 }
 
-TEST(MazeGraphTest, GetVertexFromCoords)
+TEST(MazeGraphTest, GetIndexFromCoords)
 {
     MazeGraph graph(0);
     int idx1 = graph.AddVertex(1, 2);
     ASSERT_EQ(graph.GetIndexFromCoords(1, 2), idx1);
-}
-
-TEST(MazeGraphTest, MapToGraph)
-{
-    MazeMap lab(3, 3, {{0, 0}, {0, 2}, {2, 1}, {2, 2}});
-    MazeGraph graph = lab.ToMazeGraph();
-
-    // (1, 1) should be connected to (0, 1), (1, 0) and (2, 1)
-    auto adj2 = graph.GetAdjacentNodeIndicies(graph.GetIndexFromCoords(1, 1));
-    ASSERT_TRUE(VEC_CONTAINS(adj2, graph.GetIndexFromCoords(1, 0)));
-    ASSERT_TRUE(VEC_CONTAINS(adj2, graph.GetIndexFromCoords(0, 1)));
-    ASSERT_TRUE(VEC_CONTAINS(adj2, graph.GetIndexFromCoords(1, 2)));
 }
